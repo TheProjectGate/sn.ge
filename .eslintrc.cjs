631dbc2a51237063c21b63a3cc196b9f5dfd0f72
TheProjectGate <theprojectgate2025@gmail.com>
2025-10-21 13:21:09 +0400

    Улучшена анимация в 'What We Offer' и сделана зависимой от скролла

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked', // Используем правила с проверкой типов
+    'plugin:react-hooks/recommended',
+    'react-app',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true, // Указываем, что нужно найти ближайший tsconfig.json
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
